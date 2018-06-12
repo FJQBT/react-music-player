c3263667a4df9e3f134efb900255148966259616
fjq <fjqrubbish@gmail.com>
2018-06-13 00:31:27 +0800

    add static files

---
+++ app/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './header.less'
+
+class header extends Component {
+    render() {
+        return (
+            <div className="components-header">
+                
+            </div>
+        );
+    }
+}
+
+export default header;

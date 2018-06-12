69a9b065015a9975977344de2a74eadde3165763
fjq <fjqrubbish@gmail.com>
2018-06-13 01:19:28 +0800

    add jplayer

--- app/root.js
+++ app/root.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+import jPlayer from 'jplayer';
 import Header from './components/header'
 import Progress from './components/progress'
 
 class Root extends Component {
+    constructor(){
+        super();
+        this.state = {progress: '-'}
+    }
+
+    componentDidMount(){
+        $('#player').jPlayer({
+            ready: function(){
+                $(this).jPlayer('setMedia', {
+                    mp3: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
+                }).jPlayer('play');
+            },
+            supplied: 'mp3',
+            vmode: 'window',
+            muted: true
+        });
+        $('#player').bind($.jPlayer.event.timeupdate, (e) => {
+            this.setState({
+                progress: Math.round(e.jPlayer.status.currentTime)
+            });
+        });
+    }
+
     render() {
         return (
             <div>
                 <Header />
-                <Progress progress="1" />
+                <Progress progress={this.state.progress} />
             </div>
         );
     }
